40d4ccfac67720fc3e06210b0096f7e9aebb8482
OpenShift guest <aris@shore.net>
2014-05-14 15:12:21 -0400

    Fun with Strings passes all tests

--- ruby-calisthenics/lib/fun_with_strings.rb
+++ ruby-calisthenics/lib/fun_with_strings.rb
@@ -1,12 +1,31 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    test = self.gsub(/\W/, "").downcase
+    test == test.reverse
   end
+  
+  def get_words
+      #moved downcase to usage point since we need to preserve case on anagram output
+     self
+        .gsub(/\'/,"")
+        .split(/\b/)
+        .reject {|x| x=~ /^\W+$/}
+  end
+  
   def count_words
-    # your code here
+      histogram = Hash.new(0)
+        get_words.each do |word|
+          # unless word =~ /^\W+$/
+            histogram[word.downcase]+=1
+          # end
+        end
+      return histogram
   end
   def anagram_groups
-    # your code here
+    get_words.group_by do |word| 
+        word.downcase.chars.sort
+    end
+    .values
   end
 end
 
