bcd34b679964fa944177370343d0beeb759c20e8
Aris Berard <aris@shore.net>
2014-05-15 09:42:35 -0700

    PASS: attr_accessor_with_history

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -4,7 +4,13 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(_target)
+        if(@#{attr_name}_history.nil?)
+          @#{attr_name}_history = []
+        end
+        @#{attr_name}_history.push @#{attr_name}
+        @#{attr_name} = _target
+      end
     }
   end
 end
